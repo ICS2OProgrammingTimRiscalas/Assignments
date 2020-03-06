81c2a8a7af76a92ee5c04fef100b8ea36b6722f0
Tim Riscalas <timothy.riscalas@ocsbstudent.ca>
2020-03-05 23:01:48 -0500

    finished code

--- DrawingShapesTim/main.lua
+++ DrawingShapesTim/main.lua
@@ -21,11 +21,14 @@ local myHeptagon
 local verticesOfHeptagon = {0,5, 3,3, 3,1, 1.5,-2, -1.5,-2, -3,1, -3,3}
 local areaTextHeptagon
 local myPentagon
-local verticesOfPentagon
+local verticesOfPentagon = {0,3, 3,5, 3,-2, -3,-2, -3,5}
 local areaTextPentagon
 local myTrapezoid
-local verticesOfTrapezoid
+local verticesOfTrapezoid = {-2,2, 2,2, 6,-4, -6,-4}
 local areaTextTrapezoid
+local myOctagon
+local verticesOfOctagon = {-1,2, 1,2, 2,1, 2,-1, 1,-2, -1,-2, -2,-1, -2,1}
+local areaTextOctagon
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
@@ -137,14 +140,15 @@ myHeptagon.fill = paintHeptagon
 -- set the size of my heptagon
 myHeptagon:scale(25,25)
 
--- set the width od the border
-myHeptagon.strokeWidth = 1
+-- set the width of the border
+myHeptagon.strokeWidth = 0.6
+
 
 -- set the color of the border
 myHeptagon:setStrokeColor(0/255, 0/255, 0/255)
 
 -- write "heptagon" on the screen 
-areaTextHeptagon = display.newText("hexagon", 0, 0, Arial, 25)
+areaTextHeptagon = display.newText("heptagon", 0, 0, Arial, 25)
 
 -- anchor the text and set its (x,y) position
 areaTextHeptagon.anchorX = 0
@@ -154,3 +158,111 @@ areaTextHeptagon.y = display.contentHeight/4.1
 
 -- set the colour of the text
 areaTextHeptagon:setTextColor(0/255, 0/255, 0/255)
+
+-- display the pentagon
+myPentagon = display.newPolygon(display.contentWidth/5, display.contentHeight/1.3, verticesOfPentagon)
+
+-- flip the pentagon
+myPentagon:scale(1,-1)
+
+-- set the color of the pentagon
+local paintPentagon = {
+	type = "gradient",
+	color1 = {147/255, 20/255, 179/255},
+	color2 = {255/255, 255/255, 255/255},
+	directions = "up"
+}
+myPentagon.fill = paintPentagon
+
+-- set the size of my pentagon
+myPentagon:scale(20,20)
+
+-- set the width of the border
+myPentagon.strokeWidth = 0.6
+
+-- set the color of the border
+myPentagon:setStrokeColor(0/255, 0/255, 0/255)
+
+-- write "pentagon" on the screen 
+areaTextPentagon = display.newText("pentagon", 0, 0, Arial, 25)
+
+-- anchor the text and set its (x,y) position
+areaTextPentagon.anchorX = 0
+areaTextPentagon.anchorY = 0
+areaTextPentagon.x = display.contentWidth/6.6
+areaTextPentagon.y = display.contentHeight/1.3
+
+-- set the colour of the text
+areaTextPentagon:setTextColor(0/255, 0/255, 0/255)
+
+-- display the trapezoid
+myTrapezoid = display.newPolygon(display.contentWidth/2, display.contentHeight/1.3, verticesOfTrapezoid)
+
+-- flip the trapezoid
+myTrapezoid:scale(1,-1)
+
+-- set the color of the trapezoid
+local paintTrapezoid = {
+	type = "gradient",
+	color1 = {243/255, 0/255, 0/255},
+	color2 = {255/255, 255/255, 255/255},
+	directions = "up"
+}
+myTrapezoid.fill = paintTrapezoid
+
+-- set the size of my trapezoid
+myTrapezoid:scale(20,20)
+
+-- set the width of the border
+myTrapezoid.strokeWidth = 0.6
+
+-- set the color of the border
+myTrapezoid:setStrokeColor(0/255, 0/255, 0/255)
+
+-- write "trapezoid" on the screen 
+areaTextTrapezoid = display.newText("trapezoid", 0, 0, Arial, 25)
+
+-- anchor the text and set its (x,y) position
+areaTextTrapezoid.anchorX = 0
+areaTextTrapezoid.anchorY = 0
+areaTextTrapezoid.x = display.contentWidth/2.2
+areaTextTrapezoid.y = display.contentHeight/1.3
+
+-- set the colour of the text
+areaTextTrapezoid:setTextColor(0/255, 0/255, 0/255)
+
+-- display the octagon
+myOctagon = display.newPolygon(display.contentWidth/1.2, display.contentHeight/1.3, verticesOfOctagon)
+
+-- flip the octagon
+myOctagon:scale(1,-1)
+
+-- set the color of the octagon
+local paintOctagon = {
+	type = "gradient",
+	color1 = {229/255, 229/255, 27/255},
+	color2 = {255/255, 255/255, 255/255},
+	directions = "up"
+}
+myOctagon.fill = paintOctagon
+
+-- set the size of my octagon
+myOctagon:scale(30,30)
+
+-- set the width of the border
+myOctagon.strokeWidth = 1
+
+-- set the color of the border
+myOctagon:setStrokeColor(0/255, 0/255, 0/255)
+
+-- write "octagon" on the screen 
+areaTextOctagon = display.newText("octagon", 0, 0, Arial, 25)
+
+-- anchor the text and set its (x,y) position
+areaTextOctagon.anchorX = 0
+areaTextOctagon.anchorY = 0
+areaTextOctagon.x = display.contentWidth/1.25
+areaTextOctagon.y = display.contentHeight/1.3
+
+-- set the colour of the text
+areaTextOctagon:setTextColor(0/255, 0/255, 0/255)
