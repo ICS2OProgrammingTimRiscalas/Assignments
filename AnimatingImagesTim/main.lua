dabfdbfaa90d2195cc7243546dc763423152f710
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2020-03-10 20:31:45 -0400

    added extra functionalities to AnimatingImages

--- AnimatingImagesTim/main.lua
+++ AnimatingImagesTim/main.lua
@@ -8,10 +8,14 @@
 -- -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+local backgroundSound = audio.loadSound("Sounds/creepy space sound.mp3")
+local backgroundSoundChannel = audio.play(backgroundSound,{loops = -1})
+
 -- global variables
 scrollSpeedMoon = 3.5
-scrollSpeedAstronaut = 4.0
-scrollSpeedRocketShip = 3.8
+scrollSpeedAstronaut = 3.75
+scrollSpeedRocketShip = 4.2
+parabolicCurve = 0.1
 
 -- background image with width and height
 local backgroundImage = display.newImageRect("Images/background.jfif", 2048, 1536)
@@ -33,6 +37,9 @@ moon.y = display.contentHeight/3
 local function MoveMoon(event)
 	-- add the scroll speed to the x-value of the ship
 	moon.x = moon.x + scrollSpeedMoon
+	parabolicCurve = parabolicCurve + 0.001
+	-- make the moon travel on a parabolic curve
+	moon.y = moon.x * parabolicCurve
 	-- change the transparency of the ship every time it moves so that it fades in
 	moon.alpha = moon.alpha + 0.01
 end
@@ -56,11 +63,27 @@ astronaut.y = display.contentHeight * 1/1.3
 -- Description: This function adds the scroll speed to the x-value of the astronaut
 local function MoveAstronaut(event)
 	-- add the scroll speed to the x-value of the ship
-	astronaut.x = astronaut.x - scrollSpeedAstronaut
-	-- change the transparency of the ship every time it moves so that it fades out
-	astronaut.alpha = astronaut.alpha - 0.0001  
-	-- make the astronaut grow in size
-	astronaut:scale(1.002, 1.002)
+	if (astronaut.x >= 1024) or (Hitwall1 == true) then
+		astronaut.x = astronaut.x - scrollSpeedAstronaut
+		-- change the transparency of the ship every time it moves so that it fades out
+		astronaut.alpha = astronaut.alpha - 0.001  
+		-- make the astronaut grow in size and turn back when it hits the far wall
+		astronaut.width = astronaut.width + 1
+		astronaut.height = astronaut.height + 1
+		if (astronaut.alpha <= 0.132) then
+			Hitwall1 = false
+		else
+			Hitwall1 = true
+		end
+	else
+		-- change the transparency of the ship every time it moves so that it fades out
+		astronaut.alpha = astronaut.alpha + 0.01  
+		-- make the astronaut grow in size
+		astronaut.width = astronaut.width - 1
+		astronaut.height = astronaut.height - 1
+		astronaut.x = astronaut.x + scrollSpeedAstronaut
+	end
+
 end
 
 -- MoveAstronaut will be called over and over again
@@ -83,11 +106,15 @@ rocketShip.y = display.contentHeight/1
 local function MoveRocketShip(event)
 	-- add the scroll speed to the x-value of the ship
 	rocketShip.x = rocketShip.x - scrollSpeedRocketShip
+	rocketShip.y = rocketShip.y - scrollSpeedRocketShip
 	-- change the transparency of the ship every time it moves so that it fades in
 	rocketShip.alpha = rocketShip.alpha + 0.01 
 	-- make the rocketShip grow in size
 	rocketShip:scale(1.003, 1.003)
-	rocketShip:scale(1,1)
+	if (rocketShip.x <= 0) then
+		Runtime:removeEventListener("enterFrame", MoveRocketShip)
+	end
+	
 end
 
 -- MoveRocketShip will be called over and over again
