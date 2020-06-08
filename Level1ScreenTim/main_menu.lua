f5e8366fa52015b6640a8f96a0921195a791e8c5
Tim Riscalas <timothy.riscalas@ocsbstudent.ca>
2020-06-08 16:25:39 -0400

    added level1 screen

---
+++ Level1ScreenTim/main_menu.lua
@@ -0,0 +1,455 @@
+-----------------------------------------------------------------------------------------
+--
+-- main_menu.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This is the main menu, displaying the credits, instructions & play buttons.
+-----------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------
+-- INITIALIZATIONS
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Use Widget Library
+local widget = require( "widget" )
+
+-----------------------------------------------------------------------------------------
+
+-- Naming Scene
+sceneName = "main_menu"
+
+-----------------------------------------------------------------------------------------
+
+-- Creating Scene Object
+local scene = composer.newScene( sceneName )
+
+-----------------------------------------------------------------------------------------
+-- GLOAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+-- make the sound effect
+bkgSound = audio.loadSound("Sounds/bkg sound.mp3")
+
+-- variable to see whether the bubbles have been blown
+blown = false
+
+-- variable that stops the bubbles 
+stop = false
+
+-- make the timer 
+TimerOn = false
+
+-- variable that is responsible for muting/unmuting the sound 
+soundOn = true
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+-- the local variables for this scene 
+local bkg_image
+local playButton
+local creditsButton
+local instructionsButton
+local submarine
+local bubbles
+local randomTime
+local muteButton
+local unMuteButton
+
+-----------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- Creating Transition Function to Credits Page
+local function CreditsTransition( )
+    StopBubbles()
+    timer.cancel(MoveSubmarineTimer)
+    if (ShowBubblesTimer) then
+        timer.cancel(ShowBubblesTimer)   
+    end
+    composer.gotoScene( "credits_screen", {effect = "fade", time = 500})
+end 
+
+-----------------------------------------------------------------------------------------
+-- Creating Transition to Level1 Screen
+local function Level1ScreenTransition( )
+    timer.cancel(MoveSubmarineTimer)
+    timer.cancel(resetTimer)
+    timer.cancel(blowTimer)
+    timer.cancel(ShowBubblesTimer)
+    -- stop all channels 
+    audio.stop()
+    -- transition to the level1 screen 
+    composer.gotoScene( "level1_screen", {effect = "zoomInOutFadeRotate", time = 1000})
+end    
+
+-----------------------------------------------------------------------------------------
+
+-- Creating Transition Function to Instructions Page
+local function InstructionsTransition( )
+    StopBubbles()
+    timer.cancel(MoveSubmarineTimer)
+    if (ShowBubblesTimer) then
+        timer.cancel(ShowBubblesTimer)   
+    end
+    composer.gotoScene( "instructions_screen", {effect = "flip", time = 500})
+end 
+
+-----------------------------------------------------------------------------------------
+
+local function Mute (touch)
+    if (touch.phase == "ended") then 
+        -- pause the music
+        audio.pause(bkgSoundChannel)
+        -- turn the sound variable off 
+        soundOn = false
+        -- make the muteButton invisible 
+        muteButton.isVisible = true
+        --make the unmute button visible 
+        unMuteButton.isVisible = false
+    end
+end
+
+-----------------------------------------------------------------------------------------
+
+local function UnMute (touch)
+    if (touch.phase == "ended") then 
+        -- play the music
+        audio.resume(bkgSoundChannel)
+        -- turn the sound variable on
+        soundOn = true
+        -- make the muteButton invisible 
+        muteButton.isVisible = false
+        --make the unmute button visible 
+        unMuteButton.isVisible = true
+    end
+end
+
+-----------------------------------------------------------------------------------------
+
+local function AddMuteUnMuteListeners()
+    muteButton:addEventListener("touch", UnMute)
+    unMuteButton:addEventListener("touch", Mute)
+end
+
+-----------------------------------------------------------------------------------------
+
+local function RemoveMuteUnMuteListeners()
+    muteButton:removeEventListener("touch", UnMute)
+    unMuteButton:removeEventListener("touch", Mute)
+end
+
+-----------------------------------------------------------------------------------------
+
+
+-- function that moves the submarine across the screen 
+local function MoveSubmarine( event )
+    submarine.x = submarine.x + 8
+    if (blown == false) then
+        bubbles.x = bubbles.x + 8
+    end
+    if (submarine.x == display.contentWidth) then
+        submarine.x = 0
+        bubbles.x = submarine.x + 40 
+    end
+    MoveSubmarineTimer = timer.performWithDelay(30, MoveSubmarine)
+end
+
+-----------------------------------------------------------------------------------------
+
+-- functions that stops the bubbles 
+function StopBubbles( event )
+    if (resetTimer) then
+        timer.cancel(resetTimer)
+    end
+    if (blowTimer) then
+        timer.cancel(blowTimer)
+    end
+    stop = true
+    blown = false
+    bubbles.isVisible = false
+    bubbles.y = display.contentHeight/2.7
+    bubbles.x = submarine.x + 40
+    TimerOn = false
+    setTimer()
+end
+-----------------------------------------------------------------------------------------
+
+local function StartTimer( )
+    resetTimer = timer.performWithDelay(2000, StopBubbles)
+end
+
+-----------------------------------------------------------------------------------------
+
+local function BlowBubbles( event )
+    bubbles.isVisible = true
+    blown = true
+    bubbles.y = bubbles.y - 3
+    blowTimer = timer.performWithDelay(30, BlowBubbles)
+    if (TimerOn == false) then
+        TimerOn = true
+        StartTimer()
+    end
+end
+-----------------------------------------------------------------------------------------
+function setTimer( )
+    randomTime = math.random(1000, 2000)
+    ShowBubblesTimer = timer.performWithDelay(randomTime, BlowBubbles)
+end
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+   
+    -----------------------------------------------------------------------------------------
+    -- BACKGROUND IMAGE & STATIC OBJECTS
+    -----------------------------------------------------------------------------------------
+
+    -- Insert the background image and set it to the center of the screen
+    bkg_image = display.newImage("Images/mainMenu_background.png")
+    bkg_image.x = display.contentCenterX
+    bkg_image.y = display.contentCenterY
+    bkg_image.width = display.contentWidth
+    bkg_image.height = display.contentHeight
+
+
+    -- display the submarine 
+    submarine = display.newImageRect("Images/yellowSubmarine.png",200, 200)
+    submarine.x = 0
+    submarine.y = display.contentHeight/2
+    submarine.width = 200
+    submarine.height = 200
+
+    -- display the bubbles
+    bubbles = display.newImageRect("Images/bubbles.png",50, 50)
+    bubbles.x = submarine.x + 40
+    bubbles.y = display.contentHeight/2.7
+    bubbles.width = 50
+    bubbles.height = 50
+    bubbles.isVisible = false
+
+    -- mute button 
+    muteButton = display.newImageRect ("Images/Mute Button.png", 70, 70)
+    muteButton.x = 980
+    muteButton.y = 50
+    muteButton.isVisible = false
+
+    -- unmute button 
+    unMuteButton = display.newImageRect ("Images/Unmute Button.png", 70, 70)
+    unMuteButton.x = 980
+    unMuteButton.y = 50
+    unMuteButton.isVisible = true
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( muteButton )
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( unMuteButton )
+
+    -- Associating display objects with this scene 
+    sceneGroup:insert( bkg_image )
+
+    -- Send the background image to the back layer so all other objects can be on top
+    bkg_image:toBack()
+
+    -- adding the objects to the scene 
+    sceneGroup:insert( submarine )
+
+    -- adding the objects to the scene 
+    sceneGroup:insert( bubbles )
+
+    -----------------------------------------------------------------------------------------
+    -- BUTTON WIDGETS
+    -----------------------------------------------------------------------------------------   
+
+    -- Creating Play Button
+    playButton = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentWidth*1.25/8,
+            y = display.contentHeight*7/8,
+
+            -- change the width of the button to be consistent throughout
+            width = 250,
+            height = 100,
+
+            -- Insert the images here
+            defaultFile = "Images/Play Button Unpressed.png",
+            overFile = "Images/Play Button Pressed.png",
+
+           
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = Level1ScreenTransition          
+        } )
+
+    -----------------------------------------------------------------------------------------
+
+    -- Creating Credits Button
+    creditsButton = widget.newButton( 
+        {
+            -- Set its position on the screen relative to the screen size
+            x = display.contentWidth*6.75/8,
+            y = display.contentHeight*7/8,
+           
+            -- change the width of the button to be consistent throughout
+            width = 250,
+            height = 100,
+
+            -- Insert the images here
+            defaultFile = "Images/Credits Button Unpressed.png",
+            overFile = "Images/Credits Button Pressed.png",
+
+            -- When the button is released, call the Credits transition function
+            onRelease = CreditsTransition
+        } ) 
+
+    
+    -----------------------------------------------------------------------------------------
+
+   -- Creating Instructions Button
+    instructionsButton = widget.newButton( 
+        {
+            -- Set its position on the screen relative to the screen size
+            x = display.contentWidth/2,
+            y = display.contentHeight*7/8,
+
+            -- change the width of the button to be consistent throughout
+            width = 250,
+            height = 100,
+
+            -- Insert the images here
+            defaultFile = "Images/Instructions Button Unpressed.png",
+            overFile = "Images/Instructions Button Pressed.png",
+
+            -- When the button is released, call the Credits transition function
+            onRelease = InstructionsTransition
+        } ) 
+    -----------------------------------------------------------------------------------------
+
+    -- Associating button widgets with this scene
+    sceneGroup:insert( playButton )
+    sceneGroup:insert( creditsButton )
+    sceneGroup:insert( instructionsButton )
+
+    
+    -- INSERT INSTRUCTIONS BUTTON INTO SCENE GROUP
+
+end -- function scene:create( event )   
+
+
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- see whether to pause or play bkg sound 
+    if (soundOn == true) then 
+        -- play the bkg music
+        bkgSoundChannel = audio.play(bkgSound, {channel = 1, loops = -1})
+        muteButton.isVisible = false
+        unMuteButton.isVisible = true
+    else 
+        -- pause the music 
+        audio.pause(bkgSoundChannel)
+    end
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).   
+    if ( phase == "will" ) then
+        MoveSubmarine()
+        setTimer()
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is now on screen.
+    -- Insert code here to make the scene come alive.
+    -- Example: start timers, begin animation, play audio, etc.
+    -- make function to call bkg music so there is no overlap between scenes 
+    elseif ( phase == "did" ) then
+        print(soundOn)
+
+        -- add the mute and unmute functionality to the buttons 
+        AddMuteUnMuteListeners()
+
+    end
+end -- function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    if ( phase == "will" ) then
+        -- Called when the scene is on screen (but is about to go off screen).
+        -- Insert code here to "pause" the scene.
+        -- Example: stop timers, stop animation, stop audio, etc.
+        --audio.pause(bkgSound)
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- remove mute and unmute listeners 
+        RemoveMuteUnMuteListeners()
+        -- Called immediately after scene goes off screen.
+    end
+
+end -- function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
---
+++ Level1ScreenTim/splash_screen.lua
@@ -0,0 +1,298 @@
+-----------------------------------------------------------------------------------------
+--
+-- splash_screen.lua
+-- Created by: Tim R 
+-- Date: Month Day, Year
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that I created in photoshop.
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-- Name the Scene
+sceneName = "splash_screen"
+
+-----------------------------------------------------------------------------------------
+
+-- Create Scene Object
+local scene = composer.newScene( sceneName )
+
+-- hide the status bar 
+display.setStatusBar(display.HiddenStatusBar)
+
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+local spookySounds = audio.loadSound("Sounds/spooky sounds.ogg", {channel = 1})
+local spookySoundsChannel
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES 
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local background
+local submarine1
+local submarine2
+local completeSubmarine
+local spiderSubmarine
+local spiderWeb
+local scrollXSpeed = 8
+local scrollYSpeed = -8
+local spinCounter = 0
+local spinTimer
+local spiderTimer1
+local timer1Off = false
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+-- Creating Transition Function to Main Menu Screen 
+local function MainMenuTransition( )       
+    composer.gotoScene( "main_menu", {effect = "zoomInOutFade", time = 500})
+end 
+
+local function SpiderFadeIn( )
+    if (spiderSubmarine.width < 1000) then
+        completeSubmarine.alpha = completeSubmarine.alpha - 0.1
+        spiderSubmarine.alpha = spiderSubmarine.alpha + 0.1
+        completeSubmarine.width = completeSubmarine.width - 50
+        spiderSubmarine.width = spiderSubmarine.width + 50
+        completeSubmarine.height = completeSubmarine.height - 50
+        spiderSubmarine.height = spiderSubmarine.height + 25
+        spiderWeb.height = spiderWeb.height - 5
+        timer.performWithDelay(100, SpiderFadeIn)
+    else
+        timer.performWithDelay(100, MainMenuTransition)
+    end
+end
+
+-- making the submarine move up and down the spiderWeb
+local function SpiderSubmarineDown( )
+    completeSubmarine.y = completeSubmarine.y - scrollYSpeed
+    spiderWeb.y = spiderWeb.y - scrollYSpeed
+    if (timer1Off == true) then
+        timer.performWithDelay(100, SpiderSubmarineDown)
+        if (completeSubmarine.y >= display.contentHeight/2.25) then
+            timer1Off = false
+            spiderSubmarine.isVisible = true
+            spiderSubmarine.alpha = 0
+            SpiderFadeIn()
+        end
+    end
+end
+
+-- causing the spider submarine to fade in and grow
+local function StartSpiderSubmarine( )
+    spiderWeb.isVisible = true
+    if (completeSubmarine.y >= display.contentHeight/3.5) then
+        completeSubmarine.y = completeSubmarine.y + scrollYSpeed
+        spiderWeb.y = spiderWeb.y + scrollYSpeed
+        if (timer1Off == false) then
+            spiderTimer1 = timer.performWithDelay(100, StartSpiderSubmarine)
+        end 
+    elseif (completeSubmarine.y <= display.contentHeight/2.25) then
+        timer1Off = true
+        completeSubmarine.y = completeSubmarine.y + 5
+        spiderWeb.y = spiderWeb.y + 5
+        SpiderSubmarineDown()
+    end
+end
+
+-- rotate the submarine 
+local function MoveCompleteSubmarine( )
+    completeSubmarine:rotate(scrollYSpeed)
+    if (spinCounter == 39) then
+        timer.cancel(spinTimer)
+        timer.performWithDelay(400, StartSpiderSubmarine)
+    else
+        spinCounter = spinCounter + 1
+        spinTimer = timer.performWithDelay(85, MoveCompleteSubmarine)
+    end
+end
+
+-- The function that moves the submarine1 across the screen
+local function MoveSubmarine1()
+    submarine1.x = submarine1.x + scrollXSpeed
+    submarine1.y = submarine1.y + scrollYSpeed
+    -- move the submarine1 towards the centre where it connects to submarine2 
+    if (((submarine1.x - completeSubmarine.width/3.7) < completeSubmarine.x ) and 
+        ((submarine1.x + completeSubmarine.width/3.7) > completeSubmarine.x ) and 
+        ((submarine1.y - completeSubmarine.height/3.7) < completeSubmarine.y ) and 
+        ((submarine1.y + completeSubmarine.height/3.7) > completeSubmarine.y ) ) then
+        submarine1.isVisible = false
+        completeSubmarine.isVisible = true
+    end
+end
+
+
+-- The function that moves the submarine2 across the screen
+local function MoveSubmarine2()
+    submarine2.x = submarine2.x - scrollXSpeed
+    submarine2.y = submarine2.y + scrollYSpeed
+    -- moves the submarine2 towards the centre where it connects to submarine1
+    if (((submarine2.x - completeSubmarine.width/3.7) < completeSubmarine.x ) and 
+        ((submarine2.x + completeSubmarine.width/3.7) > completeSubmarine.x ) and 
+        ((submarine2.y - completeSubmarine.height/3.7) < completeSubmarine.y ) and 
+        ((submarine2.y + completeSubmarine.height/3.7) > completeSubmarine.y ) ) then
+        submarine2.isVisible = false
+        MoveCompleteSubmarine()
+    end
+end
+
+
+
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- display the background image 
+    background = display.newImageRect("Images/splash screen background.png", display.contentWidth, display.contentHeight)
+    -- anchor the background's position
+    background.anchorX = 0
+    background.anchorY = 0
+
+    -- Insert the submarine1 image
+    submarine1 = display.newImageRect("Images/Submarine 1.png", 200, 200)
+    -- set the initial x and y position of the submarine1
+    submarine1.x = 0
+    submarine1.y = display.contentHeight
+
+    -- Insert the submarine2 image
+    submarine2 = display.newImageRect("Images/Submarine 2.png", 200, 200)
+    -- set the initial x and y position of the submarine1
+    submarine2.x = display.contentWidth
+    submarine2.y = display.contentHeight
+
+    -- Insert the completeSubmarine image 
+    completeSubmarine = display.newImageRect("Images/Complete submarine.png", 300, 200)
+    -- set the initial x and y position of the completeSubmarine
+    completeSubmarine.x = display.contentWidth/2
+    completeSubmarine.y = display.contentHeight/2.25
+    -- make the completeSubmarine invisible
+    completeSubmarine.isVisible = false
+
+    -- create the spiderWeb
+    spiderWeb = display.newRect(display.contentWidth/2, 0, 5, display.contentHeight/1.5)
+    -- make the spiderWeb invisible
+    spiderWeb.isVisible = false
+
+    -- display the spiderSubmarine 
+    spiderSubmarine = display.newImageRect("Images/Spider submarine.png", 500, 200)
+    -- set the initial x and y position of the spiderSubmarine
+    spiderSubmarine.x = display.contentWidth/2.08
+    spiderSubmarine.y = display.contentHeight/2.59
+    -- make the spiderSubmarine invisible
+    spiderSubmarine.isVisible = false
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert(background)
+
+    sceneGroup:insert( submarine1 )
+
+    sceneGroup:insert( submarine2 )
+    
+    sceneGroup:insert( spiderWeb )
+    
+    sceneGroup:insert( spiderSubmarine )
+
+
+end -- function scene:create( event )
+
+--------------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- start the splash screen music
+        spookySoundsChannel = audio.play(spookySounds )
+
+        -- Call the movesubmarine1 function as soon as we enter the frame.
+        Runtime:addEventListener("enterFrame", MoveSubmarine1)
+
+        -- Call the movesubmarine function as soon as we enter the frame.
+        Runtime:addEventListener("enterFrame", MoveSubmarine2)      
+        
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is on screen (but is about to go off screen).
+    -- Insert code here to "pause" the scene.
+    -- Example: stop timers, stop animation, stop audio, etc.
+    if ( phase == "will" ) then  
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called immediately after scene goes off screen.
+    elseif ( phase == "did" ) then
+        
+        -- stop the jungle sounds channel for this screen
+        audio.stop(spookySoundsChannel)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
